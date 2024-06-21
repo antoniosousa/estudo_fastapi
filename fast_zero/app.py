4ac46de13424058b74cc79be40bcd4f47519b749
Antonio Sousa <antonioufpi@gmail.com>
2024-06-21 02:54:44 -0300

    crud de usuários sem banco de dados

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
-from .schemas import Message
+from fastapi import FastAPI, HTTPException
+
+from .schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -8,3 +10,54 @@ app = FastAPI()
 @app.get('/', response_model=Message)
 def read_root():
     return {'mensagem': 'Olá mundo!'}
+
+
+database = []
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return UserPublic(
+        id=user_with_id.id,
+        username=user_with_id.username,
+        email=user_with_id.email,
+    )
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    return database[user_id - 1]
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    del database[user_id - 1]
+
+    return {'mensagem': 'User deleted'}
